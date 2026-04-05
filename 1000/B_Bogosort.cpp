8c0947244d3258921369ee466e72646fecda0231
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-04-05 05:49:48 +0530

    reverse

---
+++ 1000/B_Bogosort.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_add(ll a, ll b) { return (a % MOD + b % MOD) % MOD; }
+ll mod_sub(ll a, ll b) { return (a % MOD - b % MOD + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a % MOD * b % MOD) % MOD; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+
+    for(int i=0;i<n;i++) {
+        if(i != n-1) {
+            cout << a[i] << " ";
+        } else {
+            cout << a[i];
+        }
+    }
+    cout << "\n";
+}
+
+int main() {
+    fastio
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
